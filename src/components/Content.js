e05e1086d93fb9d0a0a7811b33d07a782e649557
atsirc <christa.hannuksela@gmail.com>
2021-08-10 23:28:43 +0300

    tog bort onödiga filer

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,7 @@ import Item from './Item'
 
 const Content = ({ rules, query }) => {
     return (
-      <div className="m-4" id="content" data-bs-target="#sidebar-wrapper" > 
+      <div className="m-4" id="content"> 
           { Object.entries(rules).length > 0 ? Object.entries(rules).map(([rule, text], i) => {
               return <Item key={i} header={rule} text={text} search={query}/>
             }) : query !== "" ? <span>Nothing was found with phrase <b>{query}</b></span> : <div className="spinner-border" role="status"> </div>
